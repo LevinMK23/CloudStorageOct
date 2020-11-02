688587b90aa6f268d249ca661a0b2237ed356428
zinovin <i.zinovin@mail.ru>
2020-11-02 13:17:19 +0300

    lesson1

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -1,8 +1,5 @@
 import javax.security.sasl.SaslException;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.net.Socket;
 import java.net.SocketException;
 
@@ -41,7 +38,26 @@ public class ClientHandler implements Runnable {
                     }
                 }
                 if (command.equals("download")) {
-                    // TODO: 27.10.2020
+                    try {
+                        File file = new File("server/" + in.readUTF());
+                        if (!file.exists()) {
+                            out.writeUTF("WRONG");
+                        }
+                        long length = file.length();
+                        out.writeLong(length);
+                        FileInputStream fileBytes = new FileInputStream(file);
+                        int read = 0;
+                        byte[] buffer = new byte[256];
+                        while ((read = fileBytes.read(buffer)) != -1) {
+                            out.write(buffer, 0, read);
+                        }
+                        out.flush();
+                        String status = in.readUTF();
+                        System.out.println(status);
+                        out.writeUTF("OK");
+                    } catch (Exception e) {
+                        out.writeUTF("WRONG");
+                    }
                 }
                 if (command.equals("exit")) {
                     System.out.println("Client disconnected correctly");
